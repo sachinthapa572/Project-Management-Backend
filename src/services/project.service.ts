f826954da130506b04b5f1016f3f2bbe7e4b505d
sachinthapa572 <121659364+sachinthapa572@users.noreply.github.com>
2025-01-17 17:32:20 +0545

    Remove unused environment configuration and refactor auth controller
    - chore:
        - Delete example environment configuration file
    
    - refactor:
        - Simplify async handler usage in auth controller
        - Improve readability by adjusting indentation and spacing

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -1,175 +1,185 @@
 import mongoose from 'mongoose';
+import { TaskStatusEnum } from '../enums/task.enum';
 import ProjectModel from '../models/project.model';
 import TaskModel from '../models/task.model';
-import { NotFoundException } from '../utils/appError';
-import { TaskStatusEnum } from '../enums/task.enum';
+import { BadRequestException, NotFoundException } from '../utils/appError';
 
 export const createProjectService = async (
-  userId: string,
-  workspaceId: string,
-  body: {
-    emoji?: string;
-    name: string;
-    description?: string;
-  }
+    userId: string,
+    workspaceId: string,
+    body: {
+        emoji?: string;
+        name: string;
+        description?: string;
+    }
 ) => {
-  const project = new ProjectModel({
-    ...(body.emoji && { emoji: body.emoji }),
-    name: body.name,
-    description: body.description,
-    workspace: workspaceId,
-    createdBy: userId,
-  });
 
-  await project.save();
+    const ExistedProject = await ProjectModel.findOne({
+        name: body.name,
+        createdBy: userId
+    });
 
-  return { project };
+    if (ExistedProject) {
+        throw new BadRequestException('Project already exists');
+    }
+
+    const project = new ProjectModel({
+        ...(body.emoji && { emoji: body.emoji }),
+        name: body.name,
+        description: body.description,
+        workspace: workspaceId,
+        createdBy: userId,
+    });
+
+    await project.save();
+
+    return { project };
 };
 
 export const getProjectsInWorkspaceService = async (
-  workspaceId: string,
-  pageSize: number,
-  pageNumber: number
+    workspaceId: string,
+    pageSize: number,
+    pageNumber: number
 ) => {
-  // Step 1: Find all projects in the workspace
+    // Step 1: Find all projects in the workspace
 
-  const totalCount = await ProjectModel.countDocuments({
-    workspace: workspaceId,
-  });
+    const totalCount = await ProjectModel.countDocuments({
+        workspace: workspaceId,
+    });
 
-  const skip = (pageNumber - 1) * pageSize;
+    const skip = (pageNumber - 1) * pageSize;
 
-  const projects = await ProjectModel.find({
-    workspace: workspaceId,
-  })
-    .skip(skip)
-    .limit(pageSize)
-    .populate('createdBy', '_id name profilePicture -password')
-    .sort({ createdAt: -1 });
+    const projects = await ProjectModel.find({
+        workspace: workspaceId,
+    })
+        .skip(skip)
+        .limit(pageSize)
+        .populate('createdBy', '_id name profilePicture -password')
+        .sort({ createdAt: -1 });
 
-  const totalPages = Math.ceil(totalCount / pageSize);
+    const totalPages = Math.ceil(totalCount / pageSize);
 
-  return { projects, totalCount, totalPages, skip };
+    return { projects, totalCount, totalPages, skip };
 };
 
 export const getProjectByIdAndWorkspaceIdService = async (
-  workspaceId: string,
-  projectId: string
+    workspaceId: string,
+    projectId: string
 ) => {
-  const project = await ProjectModel.findOne({
-    _id: projectId,
-    workspace: workspaceId,
-  }).select('_id emoji name description');
+    const project = await ProjectModel.findOne({
+        _id: projectId,
+        workspace: workspaceId,
+    }).select('_id emoji name description');
 
-  if (!project) {
-    throw new NotFoundException('Project not found or does not belong to the specified workspace');
-  }
+    if (!project) {
+        throw new NotFoundException('Project not found or does not belong to the specified workspace');
+    }
 
-  return { project };
+    return { project };
 };
 
 export const getProjectAnalyticsService = async (workspaceId: string, projectId: string) => {
-  const project = await ProjectModel.findById(projectId);
+    const project = await ProjectModel.findById(projectId);
 
-  if (!project || project.workspace.toString() !== workspaceId.toString()) {
-    throw new NotFoundException('Project not found or does not belong to this workspace');
-  }
+    if (!project || project.workspace.toString() !== workspaceId.toString()) {
+        throw new NotFoundException('Project not found or does not belong to this workspace');
+    }
 
-  const currentDate = new Date();
+    const currentDate = new Date();
 
-  //USING Mongoose aggregate
-  const taskAnalytics = await TaskModel.aggregate([
-    {
-      $match: {
-        project: new mongoose.Types.ObjectId(projectId),
-      },
-    },
-    {
-      $facet: {
-        totalTasks: [{ $count: 'count' }],
-        overdueTasks: [
-          {
+    //USING Mongoose aggregate
+    const taskAnalytics = await TaskModel.aggregate([
+        {
             $match: {
-              dueDate: { $lt: currentDate },
-              status: {
-                $ne: TaskStatusEnum.DONE,
-              },
+                project: new mongoose.Types.ObjectId(projectId),
             },
-          },
-          {
-            $count: 'count',
-          },
-        ],
-        completedTasks: [
-          {
-            $match: {
-              status: TaskStatusEnum.DONE,
+        },
+        {
+            $facet: {
+                totalTasks: [{ $count: 'count' }],
+                overdueTasks: [
+                    {
+                        $match: {
+                            dueDate: { $lt: currentDate },
+                            status: {
+                                $ne: TaskStatusEnum.DONE,
+                            },
+                        },
+                    },
+                    {
+                        $count: 'count',
+                    },
+                ],
+                completedTasks: [
+                    {
+                        $match: {
+                            status: TaskStatusEnum.DONE,
+                        },
+                    },
+                    { $count: 'count' },
+                ],
             },
-          },
-          { $count: 'count' },
-        ],
-      },
-    },
-  ]);
-  // console.log(JSON.stringify(taskAnalytics)); 
+        },
+    ]);
+    // console.log(JSON.stringify(taskAnalytics));
 
-  const _analytics = taskAnalytics[0];
+    const _analytics = taskAnalytics[0];
 
-  const analytics = {
-    totalTasks: _analytics.totalTasks[0]?.count || 0,
-    overdueTasks: _analytics.overdueTasks[0]?.count || 0,
-    completedTasks: _analytics.completedTasks[0]?.count || 0,
-  };
+    const analytics = {
+        totalTasks: _analytics.totalTasks[0]?.count || 0,
+        overdueTasks: _analytics.overdueTasks[0]?.count || 0,
+        completedTasks: _analytics.completedTasks[0]?.count || 0,
+    };
 
-  return {
-    analytics,
-  };
+    return {
+        analytics,
+    };
 };
 
 export const updateProjectService = async (
-  workspaceId: string,
-  projectId: string,
-  body: {
-    emoji?: string;
-    name: string;
-    description?: string;
-  }
+    workspaceId: string,
+    projectId: string,
+    body: {
+        emoji?: string;
+        name: string;
+        description?: string;
+    }
 ) => {
-  const { name, emoji, description } = body;
+    const { name, emoji, description } = body;
 
-  const project = await ProjectModel.findOne({
-    _id: projectId,
-    workspace: workspaceId,
-  });
+    const project = await ProjectModel.findOne({
+        _id: projectId,
+        workspace: workspaceId,
+    });
 
-  if (!project) {
-    throw new NotFoundException('Project not found or does not belong to the specified workspace');
-  }
+    if (!project) {
+        throw new NotFoundException('Project not found or does not belong to the specified workspace');
+    }
 
-  if (emoji) project.emoji = emoji;
-  if (name) project.name = name;
-  if (description) project.description = description;
+    if (emoji) project.emoji = emoji;
+    if (name) project.name = name;
+    if (description) project.description = description;
 
-  await project.save();
+    await project.save();
 
-  return { project };
+    return { project };
 };
 
 export const deleteProjectService = async (workspaceId: string, projectId: string) => {
-  const project = await ProjectModel.findOne({
-    _id: projectId,
-    workspace: workspaceId,
-  });
+    const project = await ProjectModel.findOne({
+        _id: projectId,
+        workspace: workspaceId,
+    });
 
-  if (!project) {
-    throw new NotFoundException('Project not found or does not belong to the specified workspace');
-  }
+    if (!project) {
+        throw new NotFoundException('Project not found or does not belong to the specified workspace');
+    }
 
-  await project.deleteOne();
+    await project.deleteOne();
 
-  await TaskModel.deleteMany({
-    project: project._id,
-  });
+    await TaskModel.deleteMany({
+        project: project._id,
+    });
 
-  return project;
+    return project;
 };
